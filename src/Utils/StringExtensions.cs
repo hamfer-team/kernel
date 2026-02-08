14e48f2b69b602f6098875ad342f5b283d9f5cb0
seyyed habibolah Tabasi <Tabasi@DESKTOP-7338EO5>
2026-02-08 23:26:54 +0330

    add `UsePersianNumbers` string extension

--- src/Utils/StringExtensions.cs
+++ src/Utils/StringExtensions.cs
@@ -26,12 +26,28 @@ public static class StringExtensions
 
   public static string ToPersianStandard(this string src)
   {
-    // TODO : standadize characters
+    // TODO : standadize more characters
     return src
       .Replace("ي", "ی")
       .Replace("ك", "ک")
       .Replace("ة", "ه")
       .Replace("إ", "ا")
       .Replace("أ", "ا");
+  }
+
+  public static string UsePersianNumbers(this string src)
+  {
+    return src
+      .Replace("0", "۰")
+      .Replace("1", "۱")
+      .Replace("2", "۲")
+      .Replace("3", "۳")
+      .Replace("4", "۴")
+      .Replace("5", "۵")
+      .Replace("6", "۶")
+      .Replace("7", "۷")
+      .Replace("8", "۸")
+      .Replace("9", "۹")
+      ;
   }
 }
